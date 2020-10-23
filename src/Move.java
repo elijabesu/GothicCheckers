4da301b502d9224973fe5d4e80e3bfbe78d295a6
saurich <ellie@saurich.com>
2020-10-23 19:55:11 +0200

    Create Move.java

---
+++ src/Move.java
@@ -0,0 +1,76 @@
+public class Move {
+    private int originalRow;
+    private int originalColumn;
+    private int movingMan;
+    private int newRow;
+    private int newColumn;
+    private int newPositionOriginalMan;
+
+    public Move(int originalRow, int originalColumn, int movingMan, int newRow, int newColumn, int newMan) {
+        this.originalRow = originalRow;
+        this.originalColumn = originalColumn;
+        this.movingMan = movingMan;
+        this.newRow = newRow;
+        this.newColumn = newColumn;
+        this.newPositionOriginalMan = newMan;
+    }
+
+    public boolean isValid(boolean player) {
+        if (newPositionOriginalMan != 0) return false; // if the position is occupied -> NOPE
+
+        if (player) {
+            if (movingMan > 0) return false; // if the WHITE player is trying to move any of the BLACK men -> NOPE
+        } else {
+            if (movingMan < 0) return false; // if the BLACK player is trying to move any of the WHITE men -> NOPE
+        }
+
+        if (movingMan == 2 || movingMan == -2) return true; // if it's a King -> YEP
+
+        // if they are trying to move backwards -> NOPE
+        if (movingMan == -1) {
+            if (newRow > originalRow) return false;
+        }
+        if (movingMan == 1) {
+            if (newRow < originalRow) return false;
+        }
+
+        // everything else:
+        return true;
+    }
+
+    public int getOriginalRow() {
+        return originalRow;
+    }
+
+    public int getOriginalColumn() {
+        return originalColumn;
+    }
+
+    public int getMan() {
+        return movingMan;
+    }
+
+    public int getNewRow() {
+        return newRow;
+    }
+
+    public int getNewColumn() {
+        return newColumn;
+    }
+
+    public int getPositionOriginalMan() {
+        return newPositionOriginalMan;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        s.append(Columns.values()[originalColumn]);
+        s.append(originalRow);
+        s.append(" -> ");
+        s.append(Columns.values()[newColumn]);
+        s.append(newRow);
+
+        return s.toString();
+    }
+}
