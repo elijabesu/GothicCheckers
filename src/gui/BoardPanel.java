984a3c5c4b1e050fc37e1c7201d05db838cd892f
elijabesu <41390133+elijabesu@users.noreply.github.com>
2021-08-18 13:33:40 +0200

    working on BoardPanel

--- src/gui/BoardPanel.java
+++ src/gui/BoardPanel.java
@@ -1,5 +1,8 @@
 package gui;
 
+import brain.Coordinate;
+import brain.Pieces;
+
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
@@ -8,21 +11,56 @@ import java.io.IOException;
 
 public class BoardPanel extends JPanel {
     private final int size;
-    private final int squareSize;
+    private final int squareSize = 50;
     private BufferedImage yellow;
     private BufferedImage purple;
+//    private BufferedImage[][] coordinates;
+    private final Pieces[][] coordinates;
 
     public BoardPanel(int size) {
         this.size = size;
-        squareSize = 50;
+//        coordinates = new BufferedImage[size][size];
+        coordinates = new Pieces[size][size];
         try {
             yellow = ImageIO.read(getClass().getResource("resources/yellow.png"));
             purple = ImageIO.read(getClass().getResource("resources/purple.png"));
         } catch (IOException error) {
             System.out.println(error);
         }
+//        generateMen();
     }
 
+    private void generateMen() {
+        for (int row = 0; row < size; row++) {
+            for (int col = 0; col < size; col++) {
+                if (col < 2) {
+                    coordinates[row][col] = Pieces.WHITE;
+//                    coordinates[row][col] = yellow;
+                } else if (col > 5) {
+                    coordinates[row][col] = Pieces.BLACK;
+//                    coordinates[row][col] = purple;
+                } else coordinates[row][col] = Pieces.EMPTY;
+            }
+        }
+    }
+
+    public int getBoardSize() {
+        return size;
+    }
+
+    public void placeMan(Pieces man, Coordinate coordinate) {
+        coordinates[coordinate.getRow()][coordinate.getColumn()] = man;
+    }
+
+    public void removeMan(Coordinate coordinate) {
+        coordinates[coordinate.getRow()][coordinate.getColumn()] = Pieces.EMPTY;
+    }
+
+    public Pieces getCoordinate(Coordinate coordinate) {
+        return coordinates[coordinate.getRow()][coordinate.getColumn()];
+    }
+
+    // initial + calling repaint()
     public void paintComponent(Graphics graphics) {
         for (int row = 0; row < size; row++) {
             for (int col = 0; col < size; col++) {
@@ -33,8 +71,14 @@ public class BoardPanel extends JPanel {
                 int colSize = col * squareSize;
 
                 graphics.fillRect(rowSize, colSize, squareSize, squareSize);
-                if (col < 2) graphics.drawImage(yellow, rowSize + 5, colSize + 5, 40, 40,this);
-                if (col > 5) graphics.drawImage(purple, rowSize + 5, colSize + 5, 40, 40,this);
+                if (coordinates[row][col] == Pieces.WHITE) graphics.drawImage(
+                        yellow, rowSize + 5, colSize + 5,40, 40, this
+                );
+                if (coordinates[row][col] == Pieces.BLACK) graphics.drawImage(
+                        purple, rowSize + 5, colSize + 5,40, 40, this
+                );
+//                if (coordinates[row][col] != null)
+//                    graphics.drawImage(coordinates[row][col], rowSize + 5, colSize + 5,40, 40, this);
             }
         }
     }
